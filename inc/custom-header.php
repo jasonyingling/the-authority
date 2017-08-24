e3388b202ea2e8b4a17a49ed1aa0dba1ccef5b4e
Jason Yingling <yingling017@gmail.com>
2017-08-23 22:43:36 -0500

    setting custom header color to ffffff to match default

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,7 +18,7 @@
 function the_authority_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'the_authority_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
+		'default-text-color'     => 'ffffff',
 		'width'                  => 1440,
 		'height'                 => 250,
 		'flex-height'            => true,
